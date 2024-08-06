82221f0ebe8e0d92cf8e16d7ffb39109cdf22024
joakimnagornoff <joakim.nagornoff@live.se>
2024-08-06 12:18:48 +0200

    feat: config.py file

--- config.py
+++ config.py
@@ -2,10 +2,15 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+def safe_eval(input_str):
+    try:
+        return eval(input_str, {"__builtins__": None}, {})
+    except Exception as e:
+        print(f"Error evaluating input: {e}")
+        return []
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-DEX_API_URL_PAIRS = os.getenv('DEX_API_URL_PAIRS')
-DEX_API_URL_TOKENS = os.getenv('DEX_API_URL_TOKENS')
+
 WALLET_ADDRESSES = eval(os.getenv('WALLET_ADDRESSES'))
 CHAT_ID = os.getenv('CHAT_ID')
 CHECK_INTERVAL = int(os.getenv('CHECK_INTERVAL'))
